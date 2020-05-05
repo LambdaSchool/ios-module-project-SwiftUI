ab9c7af9e495a8cca02234314e80560ef50edcfb
Shawn James <shawn.james@me.com>
2020-05-05 19:30:54 -0400

    complete section 4

---
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Shawn James on 5/5/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
