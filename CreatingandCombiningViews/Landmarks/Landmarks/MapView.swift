7417ca4fe2f7d8e61c7177cddb185148773a9d94
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-05 15:27:04 -0700

    Added struct for mapkit

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,17 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Bhawnish Kumar on 5/5/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
