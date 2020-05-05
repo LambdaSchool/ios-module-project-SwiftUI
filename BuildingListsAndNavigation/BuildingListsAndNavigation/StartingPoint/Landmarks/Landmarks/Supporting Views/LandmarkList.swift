0f3234a11ebd8a30562dd3ac491744c9167e67b9
Harmony Radley <harmonyradley99@gmail.com>
2020-05-05 16:50:37 -0400

    Completed sections 6-7 and working on section 8 and 9

--- BuildingListsAndNavigation/BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/Supporting Views/LandmarkList.swift
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/Supporting Views/LandmarkList.swift
@@ -8,11 +8,15 @@
 
 import SwiftUI
 
-
 struct LandmarkList: View {
     var body: some View {
-        List(landmarkData) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
         }
     }
 }
