da70f3a196b8dc4533e05acd3a689c017ba9b6c9
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-05 13:39:25 -0700

    Added project for Creating and Combining views

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Bhawnish Kumar on 5/5/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
