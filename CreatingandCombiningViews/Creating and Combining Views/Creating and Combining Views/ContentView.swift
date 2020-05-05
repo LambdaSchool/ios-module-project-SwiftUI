b29d8fefbd9c646b4b2740536d194ead3ab66ceb
Harmony Radley <harmonyradley99@gmail.com>
2020-05-05 15:35:46 -0400

    Completed Section 1 of Creating and Combining Views

---
+++ CreatingandCombiningViews/Creating and Combining Views/Creating and Combining Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Creating and Combining Views
+//
+//  Created by Harmony Radley on 5/5/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Cheese!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
