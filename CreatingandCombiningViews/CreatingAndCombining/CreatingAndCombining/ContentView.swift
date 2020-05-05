6b22f8fa49322783b97846c9ffe9c33d46ef29e4
Brad <braddiroff@icloud.com>
2020-05-05 18:40:03 -0400

    Working on Creating and Combining

--- CreatingandCombiningViews/CreatingAndCombining/CreatingAndCombining/ContentView.swift
+++ CreatingandCombiningViews/CreatingAndCombining/CreatingAndCombining/ContentView.swift
@@ -3,14 +3,25 @@
 //  CreatingAndCombining
 //
 //  Created by Bradley Diroff on 5/5/20.
-//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
