f99117f8a378f6f304bff3f9995b534351020e39
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-05 15:53:23 -0700

    Added circle image and map

--- CreatingandCombiningViews/Landmarks/Landmarks/CircleImage.swift
+++ CreatingandCombiningViews/Landmarks/Landmarks/CircleImage.swift
@@ -13,6 +13,7 @@ struct CircleImage: View {
     var body: some View {
         Image("turtlerock").clipShape(Circle()).overlay(Circle().stroke(Color.white, lineWidth: 4)).shadow(radius: 10)
         
+        
     }
 }
 
