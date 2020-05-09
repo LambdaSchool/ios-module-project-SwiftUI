3a7e466a7aac9238804de0b280a93d6fa99ae05b
Chad Parker <chad@chadparker.com>
2020-05-08 18:49:32 -0700

    First project added, section 3 done

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Chad Parker on 5/8/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
