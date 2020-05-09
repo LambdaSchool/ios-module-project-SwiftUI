8a593528684b4b8aeed1df26c276096dc9a83b94
Libby Thomas <thomaslibby05@gmail.com>
2020-05-08 22:18:40 -0500

    Code complete, project not functioning
    Multiple errors with references to json data file

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/Resources/UserData.swift
@@ -0,0 +1,9 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Elizabeth Thomas on 5/8/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import Foundation
---
+++ CreatingandCombiningViews/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,9 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Elizabeth Thomas on 5/8/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import Foundation
