74a819bfa05b5ab460d6e4b6ab3b64b6605bb713
hai666l <0xconr@protonmail.com>
2020-05-08 21:52:53 -0500

    Building Lists and Navigation

--- BuildingListsAndNavigation/BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/MapView.swift
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/MapView.swift
@@ -1,22 +1,23 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view that hosts an `MKMapView`.
-*/
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by conner on 5/8/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
 
 import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
 
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(
-            latitude: 34.011_286, longitude: -116.166_868)
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         uiView.setRegion(region, animated: true)
     }
@@ -24,6 +25,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
