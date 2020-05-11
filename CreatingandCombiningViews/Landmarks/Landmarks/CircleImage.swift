3b5c72b3affe1fbd708fd43b6d3dab60a0f2a16c
Thomas Dye <thomasallendye@gmail.com>
2020-05-10 21:45:50 -0400

    Circle Image Complete

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Thomas Dye on 5/10/20.
+//  Copyright © 2020 Thomas Dye. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+        .clipShape(Circle())
+        .overlay(
+            Circle().stroke(Color.gray, lineWidth: 4)
+        )
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
