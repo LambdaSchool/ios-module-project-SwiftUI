43c0cf78a01148f85b0b0f05bd06177e44cd7574
Dahna <dbuenrostro09@gmail.com>
2020-06-02 19:28:13 -0400

    mvp

---
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LankmarkList.swift
+//  Landmarks
+//
+//  Created by Dahna on 6/2/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LankmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+            
+        }
+    }
+}
