ae732e5620f2d3ba2968d16ffcefb08fe3c495f3
Cody Morley <codymorleyinc@gmail.com>
2020-06-02 15:10:35 -0700

    pause for Q&A

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Cody Morley on 6/2/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+    .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
