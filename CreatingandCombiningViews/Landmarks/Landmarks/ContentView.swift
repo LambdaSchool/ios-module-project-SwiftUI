38358bf7b97e147a3bbaaac5c8bfc883d89143e1
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-10 10:38:45 -0700

    Xcode project created for Creating and Combining Views

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Clayton Watkins on 7/10/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
