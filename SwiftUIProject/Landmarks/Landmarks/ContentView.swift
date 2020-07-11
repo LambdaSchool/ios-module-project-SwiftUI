10bf5376286aa64eb6d8e8bfbd634208acd1cb41
James McDougall <jameswiliammcdougall@gmail.com>
2020-07-10 20:51:58 -0500

    Project Creation

---
+++ SwiftUIProject/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Lambda_School_loaner_226 on 7/10/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
