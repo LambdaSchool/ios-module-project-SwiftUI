50d6911343fcb35d79e94e1fee4ee61f5d60ba13
morgan smith <mssmitty07@gmail.com>
2020-07-10 23:12:25 -0400

    final handling user input

---
+++ HandlingUserInput/HandlingUserInput/StartingPoint/Landmarks/Landmarks/UserData.swift
@@ -0,0 +1,16 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Morgan Smith on 7/10/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
+
