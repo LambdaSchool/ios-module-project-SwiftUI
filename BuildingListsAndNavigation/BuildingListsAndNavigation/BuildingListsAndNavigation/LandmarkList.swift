4895839407f8851352e790f3c093f2d54f859d1e
johnmccants002 <johnmccants002@gmail.com>
2020-09-09 16:00:06 -0700

    Finished tutorial on Handling User Input. Combined it with the Building Lists File

--- BuildingListsAndNavigation/BuildingListsAndNavigation/BuildingListsAndNavigation/LandmarkList.swift
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/BuildingListsAndNavigation/LandmarkList.swift
@@ -10,11 +10,24 @@ import Foundation
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject private var userData: UserData
+    
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Show Favorites Only")
+                }
+                
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(
+                            destination: LandmarkDetail(landmark: landmark)
+                                .environmentObject(self.userData)
+                        ) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
@@ -22,12 +35,13 @@ struct LandmarkList: View {
     }
 }
 
-struct LandmarkList_Previews: PreviewProvider {
+struct LandmarksList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
         }
+        .environmentObject(UserData())
     }
 }
--- BuildingListsAndNavigation/BuildingListsAndNavigation/BuildingListsAndNavigation/SceneDelegate.swift
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/BuildingListsAndNavigation/SceneDelegate.swift
@@ -28,11 +28,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
-                let window = UIWindow(windowScene: windowScene)
-                window.rootViewController = UIHostingController(rootView: LandmarkList())
-                self.window = window
-                window.makeKeyAndVisible()
-            }
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(
+                rootView: LandmarkList()
+                    .environmentObject(UserData())
+            )
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
